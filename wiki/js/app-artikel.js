5d3157c03be72450920b21c8a7270ae119b73342
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-12-04 13:56:15 +0100

    app-artikel

---
+++ wiki/js/app-artikel.js
@@ -0,0 +1,26 @@
+import { css,LitElement, html } from 'lit-element';
+import '@intcreator/markdown-element';
+export class appArtikel extends LitElement {
+	static get properties() {
+		return {
+			src: {type:String},
+			_title: {type:String},
+			_content: {type:String}
+		};
+	}
+	constructor() {
+		super();
+		this.src='';
+	}
+	render() {
+		return html`<h1>${this._title}</h1>
+		<markdown-element markdown="${this._content}"></markdown-element>`
+	}
+	set src(val) {
+		fetch(`articles/${val}`).then(response => response.json()).then(response => {
+			this._content = response.data;
+			this._title = response.title;
+		})
+	}
+}
+customElements.define('app-artikel', appArtikel)
