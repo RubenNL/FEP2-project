8665176c7cc9fe5644ff26bb0df7efaa60ee9bf1
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-12-07 16:02:48 +0100

    style aanpassingen

--- wiki/js/app-sidebar.js
+++ wiki/js/app-sidebar.js
@@ -11,7 +11,11 @@ export class appSidebar extends LitElement{
 	constructor() {
 		super();
 		this.logo = '';
-		this.links = [];
+		this.links = [
+			{'text' : 'Google', 'href' : 'https://www.google.com/'},
+			{'text' : 'Reddit', 'href' : 'https://www.reddit.com/'},
+			{'text' : 'Youtube', 'href' : 'https://www.youtube.com/'}
+		];
 	}
 
 	static get styles(){
@@ -19,17 +23,32 @@ export class appSidebar extends LitElement{
 		return css`
 			:host{
 				display:block;
-                background-color:lightgrey;
+                background-color:#fff;
                 border-radius:5px;
                 padding:10px;
                 height:100%;
+                box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+			}
+			#side-nav{
+				list-style-type:none;
+				padding-left:0px;
+			}
+            #side-nav > li{
+				margin: 10px 0px;
+				font-size: 13px;
 			}
 		`;
 	}
 
 	render(){
 		return html`
-			Sidebar
+			<link rel="stylesheet" href="css/style.css">
+			<h2>Menu</h2>
+			<ul id="side-nav">
+			${this.links.map((link) => html`
+				<li><a href="${link.href}">${link.text}</a></li>
+			`)}
+			</ul>
 		`;
 	}
 }
