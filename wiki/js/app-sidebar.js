9695e19e73928acdb00790b16b8f61223335cf00
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-12-07 16:13:05 +0100

    logo property verwijderd

--- wiki/js/app-sidebar.js
+++ wiki/js/app-sidebar.js
@@ -3,7 +3,6 @@ import { LitElement,html,css} from 'lit-element';
 export class appSidebar extends LitElement{
 	static get properties(){
 		return {
-			logo : {type:String},
 			links : {type:Array}
 		}
 	}
@@ -12,22 +11,18 @@ export class appSidebar extends LitElement{
 		super();
 		this.logo = '';
 		this.links = [
-			{'text' : 'Google', 'href' : 'https://www.google.com/'},
-			{'text' : 'Reddit', 'href' : 'https://www.reddit.com/'},
-			{'text' : 'Youtube', 'href' : 'https://www.youtube.com/'}
+			{'text': 'Google', 'href': 'https://www.google.com/'},
+			{'text': 'Reddit', 'href': 'https://www.reddit.com/'},
+			{'text': 'Youtube', 'href': 'https://www.youtube.com/'}
 		];
+		this.classList.add('bg-container');
 	}
 
 	static get styles(){
 		// language=css
 		return css`
 			:host{
-				display:block;
-                background-color:#fff;
-                border-radius:5px;
-                padding:10px;
-                height:100%;
-                box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+				min-height:300px;
 			}
 			#side-nav{
 				list-style-type:none;
