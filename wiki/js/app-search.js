ff9123ff97f1df4c82483dc1f6dca952b008bcb1
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-12-07 14:25:08 +0100

    search met hard-coded articles+1 dummy article

--- wiki/js/app-search.js
+++ wiki/js/app-search.js
@@ -8,23 +8,50 @@ export class AppSearch extends LitElement {
 	}
 	constructor() {
 		super();
-		this._suggestions=[]
+		this._suggestions=[];
+		this._value='';
+	}
+	static get styles() {
+		//language=css
+		return css`
+#links {
+	display:none;
+    border-radius:5px;
+    background-color: white;
+    position: relative;
+    z-index: 1;
+    padding: 5px;
+    padding-left: 10px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+}
+:host(:hover) > #links {
+    display: block;
+}`
 	}
 	render() {
-		return html`<input @input="${this._onchange}"><ul>${this._suggestions.map(suggestion=>html`<li article="${suggestion.id}">${suggestion.text}</li>`)}</ul>`
+		return html`
+			<input @input="${this._onchange}">
+			<div id="links">
+			${this._suggestions.length>0?this._suggestions.map(suggestion=>html`<a href="#" @click="${this._openArticle(suggestion.id)}">${suggestion.text}</a><br>`):html`no results found`}
+			</div>
+		`;
 	}
 	_onchange(e) {
 		this._value=this.shadowRoot.querySelector('input')._value;
-		new Promise((resolve,reject)=>{
+		new Promise((resolve,reject)=>{//TODO met een fetch.
 			resolve([
-				{text:'abc',id:123},
-				{text:'def',id:456}
+				{text:'test',id:'demo'},
+				{text:'java',id:123}
 			])
 		}).then(items=>{
 			this._suggestions=items;
-		})
-
-		console.log(this._value);
+		});
+	}
+	_openArticle(id) {
+		return ()=>{
+			document.querySelector('app-artikel').src=`${id}.json`;//TODO mischien met event?
+		};
 	}
 }
-customElements.define('app-search', AppSearch)
+customElements.define('app-search', AppSearch);
