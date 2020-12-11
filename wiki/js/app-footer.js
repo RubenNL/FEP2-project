ab308198d1660f41124836c730df73586e300de8
NickdeGooijer <34306846+NickdeGooijer@users.noreply.github.com>
2020-12-11 09:52:19 +0100

    updated footer again

--- wiki/js/app-footer.js
+++ wiki/js/app-footer.js
@@ -26,8 +26,8 @@ export class appFooter extends LitElement {
 
     constructor() {
         super();
-        this._avansLink = { src: ""};
-        this._hboiLink = { src: ""};
+        this._avansLink = { src: "https://www.avans.nl/"};
+        this._hboiLink = { src: "https://www.hbo-i.nl/"};
         this._githubImage = { src: " http://pngimg.com/uploads/github/github_PNG85.png"};
         this._githubLink = { src: "https://github.com/HU-SD-SV2PRFED-studenten-2021/prfed_2021-v2b-v"};
         this._huLink = { src: "https://hu/nl"};
