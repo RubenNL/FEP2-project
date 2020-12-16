18be242112cc0bd5d504a95a05751ec4d8b68d8a
GianniG <xlightningclawx110402@gmail.com>
2020-12-16 11:13:30 +0100

    router aanpassingen merge

--- wiki/js/app-inlog.js
+++ wiki/js/app-inlog.js
@@ -6,6 +6,7 @@ export class appInlog extends LitElement {
         return {
             _username: {type:String},
             _password: {type:String},
+            location: Object
         }
     }
     constructor() {
