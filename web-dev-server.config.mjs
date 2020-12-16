d95601fb6bf46c8793dc78427928306665feb2f6
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-12-16 10:26:14 +0100

    remove /wiki part from URL

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -5,5 +5,9 @@ export default {
 		proxy('/api', {
 			target: 'http://localhost:7999',
 		}),
+		function rewriteToWiki(context, next) {
+			if(!context.url.includes('node_modules')) context.url='/wiki'+context.url
+			return next();
+		},
 	],
 };
