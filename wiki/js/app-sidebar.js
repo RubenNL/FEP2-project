c9bc0db0af887fc19f2e81d6428ec35e227bff2a
NickdeGooijer <34306846+NickdeGooijer@users.noreply.github.com>
2020-12-18 14:06:31 +0100

    new article button

--- wiki/js/app-sidebar.js
+++ wiki/js/app-sidebar.js
@@ -16,7 +16,7 @@ export class appSidebar extends LitElement {
         })
         this.classList.add('bg-container');
     }
-  
+
     static get styles() {
         // language=css
         return css`
@@ -24,42 +24,72 @@ export class appSidebar extends LitElement {
                 min-height: 300px;
                 color: #383838;
             }
-            
+
             #side-nav {
                 list-style-type: none;
                 padding-left: 0px;
                 color: #383838;
             }
 
-            #side-nav-sub > a{
+            #side-nav-sub > a {
                 text-decoration: none;
                 color: #383838;
                 padding-bottom: 10px;
             }
-            
+
 
             #side-nav-sub {
                 padding-left: 40px;
-            } 
-            .head-item{
+            }
+
+            .head-item {
                 margin: 10px 0px;
                 font-size: 20px;
                 font-weight: bold;
             }
-            .sub-item > *{
+
+            .sub-item > * {
                 text-decoration: none;
                 list-style-type: square;
                 font-size: 16px;
                 color: #383838;
             }
-            .sub-item{
+
+            .sub-item {
                 margin-bottom: 5px;
                 height: 20px;
             }
-            
-            .sub-item :hover{
+
+            .button {
+                display: flex;
+                color: inherit; /* blue colors for links too */
+                text-decoration: inherit; /* no underline */
+                display: inline-block;
+                padding: 1px 5px 2px;
+                background: ButtonFace;
+                color: ButtonText;
+                border-style: solid;
+                border-width: 2px;
+                border-color: ButtonHighlight ButtonShadow ButtonShadow ButtonHighlight;
+                border-radius: 5px;
+            }
+
+            .sub-item :hover {
                 color: whitesmoke;
                 background-color: darkgray;
+            }
+            .button{
+                display: block;
+                background: #0066c4;
+                color: #ffffff;
+                cursor: pointer;
+                border: 0;
+                transition: all 0.5s;
+                border-radius: 3px;
+                align-self: end;
+                grid-column: 1;
+                text-align: center;
+                padding: 5px 20px;
             }
         `;
     }
@@ -68,6 +98,7 @@ export class appSidebar extends LitElement {
         return html`
             <link rel="stylesheet" href="/css/style.css">
             <h2>Menu</h2>
+            <a router-link href="/creator" class="button" id="newArticleButton">Nieuw artikel</a>            
             <ul id="side-nav">
                 ${this._categories.map((hoofdcat) => html`
                     <li class="head-item">${hoofdcat.headcatagory}</li>
