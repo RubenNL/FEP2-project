63aeb03ca8a805f78c3c3b1f6f58ad2918d1e359
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-01-04 12:08:25 +0100

    spaces to tabs

--- wiki/js/app-category-page.js
+++ wiki/js/app-category-page.js
@@ -1,43 +1,43 @@
 import {css, LitElement, html} from 'lit-element';
 
 export class appCategoryPage extends LitElement {
-    static get properties() {
-        return {
-            _id: {type: String},
-            _articles: {type: Array},
-            location: Object,
-            src: {type: Number},
-            _subcategoryName: {type: String},
-            _headcategoryName: {type: String}
-        };
-    }
+	static get properties() {
+		return {
+			_id: {type: String},
+			_articles: {type: Array},
+			location: Object,
+			src: {type: Number},
+			_subcategoryName: {type: String},
+			_headcategoryName: {type: String}
+		};
+	}
 
-    constructor() {
-        super();
-        this._articles = [];
-    }
+	constructor() {
+		super();
+		this._articles = [];
+	}
 
-    render() {
-        //language=HTML;
-        return html`<h2>${this._headcategoryName}</h2>
-        <h3>${this._subcategoryName}</h3>
-        <div class="articlecontainer">
-            ${this._articles.map((artikel) => html`<a class="article" router-link href="/article/${artikel.id}">
-                <h4>${artikel.title}</h4>
-                <p>${artikel.preview}</p>
-                </a>`
-            )}
-        </div>`
-    }
+	render() {
+		//language=HTML;
+		return html`<h2>${this._headcategoryName}</h2>
+		<h3>${this._subcategoryName}</h3>
+		<div class="articlecontainer">
+			${this._articles.map((artikel) => html`<a class="article" router-link href="/article/${artikel.id}">
+						<h4>${artikel.title}</h4>
+						<p>${artikel.preview}</p>
+					</a>`
+			)}
+		</div>`
+	}
 
-    static get styles() {
-        // language=css
-        return css`
+	static get styles() {
+		// language=css
+		return css`
             .articlecontainer{
                 display: grid;
                 grid-template-columns: 1fr 1fr 1fr;
             }
-            
+
             .article{
                 background-color: #fff;
                 width: auto;
@@ -59,29 +59,29 @@ export class appCategoryPage extends LitElement {
                 margin:0;
                 font-size:13px;
             }
-            
+
             .article > *{
                 text-align: left;
                 color:#000;
             }
-        `}
+		`}
 
-    onBeforeEnter(location, commands, router) {
-        this.src = location.params.categoryID
-        fetch(`/api/getCategory/${location.params.categoryID}`).then(response => response.json()).then(response => {
-            this._subcategoryName = response.name;
-            this._headcategoryName = response.headcatagory;
-            console.log(this._headcategoryName);
-        })
+	onBeforeEnter(location, commands, router) {
+		this.src = location.params.categoryID
+		fetch(`/api/getCategory/${location.params.categoryID}`).then(response => response.json()).then(response => {
+			this._subcategoryName = response.name;
+			this._headcategoryName = response.headcatagory;
+			console.log(this._headcategoryName);
+		})
 
-    }
+	}
 
-    set src(val) {
-        fetch(`/api/getArticlesByCategory/${val}`).then(response => response.json()).then(response => {
-            this._articles = response;
-            console.log(this._articles);
-        })
-    }
+	set src(val) {
+		fetch(`/api/getArticlesByCategory/${val}`).then(response => response.json()).then(response => {
+			this._articles = response;
+			console.log(this._articles);
+		})
+	}
 }
 
 customElements.define('app-category-page', appCategoryPage)
