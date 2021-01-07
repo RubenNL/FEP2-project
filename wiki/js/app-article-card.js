ed1bf49d7de5c89c80cbacc8cf62ce7bc0103af3
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-01-07 17:00:24 +0100

    bookmarkFromCard

--- wiki/js/app-article-card.js
+++ wiki/js/app-article-card.js
@@ -1,11 +1,12 @@
 import {css, LitElement, html} from 'lit-element';
-
+import 'fa-icons'
 export class AppArticleCard extends LitElement {
     static get properties() {
         return {
             id: {type:String},
 			title:{type: String},
-			preview:{type: String}
+			preview:{type: String},
+			_bookmarked: {type:Boolean}
         };
     }
 
@@ -13,10 +14,12 @@ export class AppArticleCard extends LitElement {
         super();
 		this.title='';
 		this.preview='';
+		this._bookmarked=false;
     }
     firstUpdated() {
+		this.id=''+this.id;
+		this._bookmarked=JSON.parse(window.localStorage.getItem('bookmarks')||'[]').includes(this.id)
 		fetch(`/api/getArticlePreview/${this.id}`).then(response=>response.json()).then(article=>{
-			this.id=article.id;
 			this.title=article.title;
 			this.preview=article.preview;
 		})
@@ -58,18 +61,37 @@ export class AppArticleCard extends LitElement {
             .article > *{
                 text-align: left;
                 color:#000;
-            }`
+            }
+            #toggleBookmark {
+				float:right;
+			}`
     }
 
     render(){
         //language=HTML
         return html`
             <a class="article" router-link href="/article/${this.id}">
+				${window.localStorage.getItem('user')?html`
+				<a id="toggleBookmark" tabindex="0" title="toggle bookmark" @click="${this.bookmark}">${
+					this._bookmarked
+					?html`<fa-icon class="fas fa-bookmark" path-prefix="/node_modules"/>`
+					:html`<fa-icon class="far fa-bookmark" path-prefix="/node_modules"/>`
+				}</a>`:html``}
 				<h4>${this.title}</h4>
 				<p>${this.preview}</p>
 			</a>
         `;
     }
+    bookmark(e) {
+		e.preventDefault();
+		this._bookmarked=!this._bookmarked;
+		let bookmarks=JSON.parse(window.localStorage.getItem('bookmarks')||'[]')
+		if(this._bookmarked) bookmarks.push(this.id)
+		else bookmarks.splice(bookmarks.indexOf(this.id), 1)
+		window.localStorage.setItem('bookmarks',JSON.stringify(bookmarks))
+		sendAuthenticated('/api/setBookmarks',bookmarks)
+		this.requestUpdate();
+	}
 }
 customElements.define('app-article-card', AppArticleCard)
  
