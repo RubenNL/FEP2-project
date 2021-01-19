b44064a4b6e1af7816931255e85d409bf9493abc
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-01-19 11:19:57 +0100

    indent en css fix

--- wiki/js/app-manage-categories.js
+++ wiki/js/app-manage-categories.js
@@ -25,29 +25,27 @@ export class appManageCategories extends connect(store)(LitElement) {
 		return html`
             <h2>Categorieën beheren</h2>
             <div id="flex-container">
-                <div class="categorycontainer">
+                <ul class="categorycontainer">
                     <h2>Categories</h2>
                     ${this._categories.map((hoofdcat) => html`
-					<div id="headcat">
+					<li id="headcat">
                         ${hoofdcat.headcatagory}
                         <li>
 							<ul class="side-nav-sub">${hoofdcat.subcatagories.map(sub => html`
-								<li class="sub-item">${sub.title})<span id="icon-holder">
+								<li class="sub-item">${sub.title}
 									<span id="icon-holder">
 										<fa-icon title="Pas naam aan" @click="${() => alert('Category naam is aangepast')}"
 										class="fas fa-pencil-alt autor"></fa-icon>
-							 
 										<fa-icon title="Verwijder subcategory" @click="${() => alert('Category is verwijderd!')}" 
 										class="fas fa-trash-alt"></fa-icon>
 									</span>
 								</li>`)}
 							</ul>
                         </li>
+					</li>
                     `)}
-                    </div>
-                </div>
-            </div>
-            		
+				</ul>
+			</div>
 		`;
 	}
 
@@ -57,46 +55,44 @@ export class appManageCategories extends connect(store)(LitElement) {
             :host {
                 min-height: 100%;
                 color: var(--text-color);
-				
             }
 			
 			#headcat {
                 font-weight: bold;
 			}
 
+            .sub-item {
+                padding-left: 10px;
+            }
+            .sub-item:hover {
+                background-color: var(--menu-hover);
+            }
+            .sub-item:hover #icon-holder{
+                transition: 0.4s all ease-in-out;
+                opacity: 1;
+            }
+			
+			#headcat,
+			.sub-item{
+                border-bottom:1px solid gray;
+			}
+
             #flex-container {
                 display: flex;
-                
             }
 
             .categorycontainer {
                 padding: 0px;
                 width: 40%;
-                margin-right: 12px;
             }
 
             ul {
                 list-style-type: none;
                 padding: 0;
-                
             }
 
             li {
-                border-bottom: 1px solid gray;
                 padding: 4px 0;
-            }
-
-            li:hover {
-                background-color: var(--menu-hover);
-            }
-
-            li:hover #icon-holder{
-                transition: 0.4s all ease-in-out;
-                opacity: 1;
-            }
-
-            .sub-item {
-                padding-left: 10px;
             }
 
             #icon-holder {
@@ -111,7 +107,6 @@ export class appManageCategories extends connect(store)(LitElement) {
                 padding: 0 3px;
                 cursor: pointer;
             }
-
             fa-icon:hover {
                 transform: scale(1.3);
             }
